5d3e76ed09de472efd27aeb9e407225f664120c6
yumiko yamaguthi <fabulous1109@icloud.com>
2021-07-18 12:09:58 +0900

    first commit

---
+++ database/migrations/2021_06_18_083424_create_admins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('login_id');
+            $table->text('name');
+            $table->text('login_pass');
+            $table->boolean('delete_flg');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
